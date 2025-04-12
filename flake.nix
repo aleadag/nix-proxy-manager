c78abfea5a619e6f2bbfa48d64ea378750119e0f
Alexander Wang <aleadag@gmail.com>
2025-04-12 23:34:15 +0800

    chore: format and add alias

--- flake.nix
+++ flake.nix
@@ -3,33 +3,46 @@
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
 
       /*
-       * Change this value ({major}.{min}) to
-       * update the Python virtual-environment
-       * version. When you do this, make sure
-       * to delete the `.venv` directory to
-       * have the hook rebuild it for the new
-       * version, since it won't overwrite an
-       * existing one. After this, reload the
-       * development shell to rebuild it.
-       * You'll see a warning asking you to
-       * do this when version mismatches are
-       * present. For safety, removal should
-       * be a manual step, even if trivial.
-       */
+        Change this value ({major}.{min}) to
+        update the Python virtual-environment
+        version. When you do this, make sure
+        to delete the `.venv` directory to
+        have the hook rebuild it for the new
+        version, since it won't overwrite an
+        existing one. After this, reload the
+        development shell to rebuild it.
+        You'll see a warning asking you to
+        do this when version mismatches are
+        present. For safety, removal should
+        be a manual step, even if trivial.
+      */
       version = "3.13";
     in
     {
-      packages = forEachSupportedSystem ({ pkgs }:
+      packages = forEachSupportedSystem (
+        { pkgs }:
         let
-          concatMajorMinor = v:
+          concatMajorMinor =
+            v:
             pkgs.lib.pipe v [
               pkgs.lib.versions.splitVersion
               (pkgs.lib.sublist 0 2)
@@ -38,8 +51,8 @@
 
           python = pkgs."python${concatMajorMinor version}";
         in
-        {
-          default = python.pkgs.buildPythonApplication rec {
+        rec {
+          nix-proxy-manager = python.pkgs.buildPythonApplication rec {
             pname = "nix-proxy-manager";
             version = "0.1";
             format = "pyproject";
@@ -54,12 +67,15 @@
               runHook postCheck
             '';
           };
+          default = nix-proxy-manager;
         }
       );
 
-      devShells = forEachSupportedSystem ({ pkgs }:
+      devShells = forEachSupportedSystem (
+        { pkgs }:
         let
-          concatMajorMinor = v:
+          concatMajorMinor =
+            v:
             pkgs.lib.pipe v [
               pkgs.lib.versions.splitVersion
               (pkgs.lib.sublist 0 2)
@@ -74,6 +90,7 @@
               python.pkgs.pip
             ];
           };
-        });
+        }
+      );
     };
 }
