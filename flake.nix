b77aa2bb9d97dd8d3914cfece7c42a50971f3387
Alexander Wang <aleadag@gmail.com>
2025-04-12 23:42:34 +0800

    chore: use default python3 from nixpkgs

--- flake.nix
+++ flake.nix
@@ -20,36 +20,12 @@
             pkgs = import nixpkgs { inherit system; };
           }
         );
-
-      /*
-        Change this value ({major}.{min}) to
-        update the Python virtual-environment
-        version. When you do this, make sure
-        to delete the `.venv` directory to
-        have the hook rebuild it for the new
-        version, since it won't overwrite an
-        existing one. After this, reload the
-        development shell to rebuild it.
-        You'll see a warning asking you to
-        do this when version mismatches are
-        present. For safety, removal should
-        be a manual step, even if trivial.
-      */
-      version = "3.13";
     in
     {
       packages = forEachSupportedSystem (
         { pkgs }:
         let
-          concatMajorMinor =
-            v:
-            pkgs.lib.pipe v [
-              pkgs.lib.versions.splitVersion
-              (pkgs.lib.sublist 0 2)
-              pkgs.lib.concatStrings
-            ];
-
-          python = pkgs."python${concatMajorMinor version}";
+          python = pkgs.python3;
         in
         rec {
           nix-proxy-manager = python.pkgs.buildPythonApplication rec {
@@ -74,15 +50,7 @@
       devShells = forEachSupportedSystem (
         { pkgs }:
         let
-          concatMajorMinor =
-            v:
-            pkgs.lib.pipe v [
-              pkgs.lib.versions.splitVersion
-              (pkgs.lib.sublist 0 2)
-              pkgs.lib.concatStrings
-            ];
-
-          python = pkgs."python${concatMajorMinor version}";
+          python = pkgs.python3;
         in
         {
           default = pkgs.mkShellNoCC {
